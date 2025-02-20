f55409bf0b8e0d7b80745d99fff045c4a2cfa6a1
cavejon <tehviperry@gmail.com>
2025-02-19 22:06:11 -0300

    New: Create Interface Login/Register

--- js/homePageJs.js
+++ js/homePageJs.js
@@ -1,14 +1,13 @@
-// Seleciona o título
 const title = document.getElementById("animated-title");
 const text = title.innerText;
-title.innerText = ""; // Limpa o título original
+title.innerText = "";
 
-// Divide cada letra em um <span>
+
 text.split("").forEach((letter, index) => {
     const span = document.createElement("span");
     span.innerText = letter;
     span.classList.add("letter");
-    span.style.animationDelay = `${index * 0.1}s`; // Adiciona atraso progressivo
+    span.style.animationDelay = `${index * 0.1}s`;
     title.appendChild(span);
 });
 
